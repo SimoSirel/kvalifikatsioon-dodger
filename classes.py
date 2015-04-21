c177f79b5394fc35cc027f3d7f110fcafb991845
simo sirel <simo.sirel@mail.ee>
2015-04-21 23:11:06 +0300

    Mäng põhimõtteliselt valmis-pildid vaja veel ära cropida

--- classes.py
+++ classes.py
@@ -3,24 +3,25 @@ __author__ = 'simo.sirel'
 import pygame,random
 
 class Ship(pygame.sprite.Sprite):
-    """Movable ship with which one dodges obstacles
+    """Movable ship with which played dodges obstacles
 	Returns: ship object
-	Functions: reinit, update, moveleft, moveright, stop
-	Attributes: speed, loc"""
-    def __init__(self,speed,loc):
+	Functions: init, reinit, update, moveleft, moveright, stop
+	Attributes: speed, location, image"""
+    def __init__(self,speed):
         pygame.sprite.Sprite.__init__(self)
+        screen = pygame.display.get_surface()
         self.image = pygame.image.load('icons/ship.png')
         self.image = pygame.transform.scale(self.image,(64,64))
-        screen = pygame.display.get_surface()
         self.rect=self.image.get_rect()
-        self.orig_rect_x=loc[0]             #tahaks kasutada width, aga see lõhub failid ära
-        self.orig_rect_y=loc[1]             #tahaks kasutada width, aga see lõhub failid ära
+        self.orig_rect_x=round(screen.get_size()[0]/2)
+        self.orig_rect_y=round(screen.get_size()[1]*8/10)
         self.speed_num = speed
         self.speed=0
         self.reinit()
 
 
     def reinit(self):
+        #used to reinitialise ship location after game restarts
         self.rect.x = self.orig_rect_x
         self.rect.y = self.orig_rect_y
 
@@ -39,22 +40,26 @@ class Ship(pygame.sprite.Sprite):
 class obstacle(pygame.sprite.Sprite):
     """Obstacles on the way of the ship
 	Returns: obstacle object
-	Functions: reinit, update, moveleft, moveright
-	Attributes: speed"""
+	Functions: init, update, rand_data_calc, stop
+	Attributes: speed, image, location, scale"""
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
+        self.screen = pygame.display.get_surface()
         self.image = pygame.image.load('icons/asteroid.png')
         self.rand_data_calc()
+
+
+    def rand_data_calc(self):
+        #random based variables for obstacle class
+        self.rand_scale = (random.randint(30,100)//10)*10
+        self.rand_speed = random.randint(3,6)
+        self.rand_loc = random.randint(0,self.screen.get_size()[0])
         self.image = pygame.transform.scale(self.image,(self.rand_scale,self.rand_scale))
         self.rect = self.image.get_rect()
         self.rect.x = self.rand_loc
 
-
-    def rand_data_calc(self):
-        self.rand_scale = (random.randint(30,100)//10)*10
-        self.rand_speed = random.randint(3,6)
-        self.rand_loc = random.randint(0,700)      #tahaks kasutada width, aga see lõhub failid ära
-
     def update(self):
+        #updates obstacle location and if obstacle gets out of screen reinitializes its variables
         self.rect.y += self.rand_speed
-
+        if self.rect.y >= self.screen.get_size()[1]:
+            self.rand_data_calc()
--- main.py
+++ main.py
@@ -1,28 +1,66 @@
 __author__ = 'simo.sirel'
 
-import pygame,classes
+import pygame,classes,time
+
+#define initial variables and start game
 pygame.init()
 running=True
-
 size=width,height=700,500
 screen = pygame.display.set_mode(size)
 
-ship=classes.Ship(5,[250,400])
+#define used font
+myfont = pygame.font.SysFont("monospace", 15)
+end=myfont.render("Press SPACE to start again or ESC to quit.", 10, [0,0,0])
+
+#define variables that are needed to redefine when the game restarts
+lives = 3
+debris = pygame.sprite.Group()
+start = time.time()
+lost = False
+for _ in range(15):
+    asteroid=classes.obstacle()
+    debris.add(asteroid)
+
+#create player class
+ship=classes.Ship(5)
 ships=pygame.sprite.Group(ship)
-asteroid=classes.obstacle()
-debris=pygame.sprite.Group(asteroid)
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: running = False
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT: ship.moveleft()
-            if event.key == pygame.K_RIGHT: ship.moveright()
+            if event.key == pygame.K_LEFT and lost == False: ship.moveleft()
+            if event.key == pygame.K_RIGHT and lost == False: ship.moveright()
+            if event.key == pygame.K_ESCAPE and lost == True: running = False
+            if event.key == pygame.K_SPACE and lost == True:
+                #if player chooses to restart game then there variables are needed to redefine
+                ship.reinit()
+                lives=3
+                debris=pygame.sprite.Group()
+                for _ in range(15):
+                    asteroid=classes.obstacle()
+                    debris.add(asteroid)
+                start = time.time()
+                lost = False
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or pygame.K_RIGHT:
                 ship.stop()
 
+    if pygame.sprite.spritecollide(ship, debris, True):
+        lives-=1
+
+
     screen.fill([255,255,255])
+
+    if lives <=0:
+        if lost == False:
+            stop = time.time()
+            score = myfont.render("you survived for "+str(round(stop-start,3))+" seconds", 10, [0,0,0])
+        lost = True
+        screen.blit(end,(200,200))
+        screen.blit(score,(200,250))
+
+    #evrything is drawn to screen
     ships.draw(screen)
     debris.draw(screen)
     ships.update()
